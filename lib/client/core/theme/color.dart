8512c184aad3907dc545111e07e73899af6fe049
ibrahimysr <ibrahimyasar2701@gmail.com>
2025-04-17 14:57:49 +0300

    .

--- lib/client/core/theme/color.dart
+++ lib/client/core/theme/color.dart
@@ -1,13 +1,6 @@
  import 'package:flutter/material.dart';
 
-// class AppColor { 
-//   static Color primary = Color(0xFFE6484E);
-//   static Color secondary = Color(0xFF069389);
-//   static Color black = Color(0xFF131111);
-//   static Color white = Color(0xFFFCFCFC);
-//   static Color greyLight = Color(0xFFBABABA);
-//   static Color? grey = Colors.grey[200];
-// }
+
 
 // class AppColor {
 //   static Color secondary = Color(0xFFCB997E);     
