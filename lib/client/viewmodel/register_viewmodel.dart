cfb95bd32dc78f7928543227f98a9b8e345cf70c
ibrahimysr <ibrahimyasar2701@gmail.com>
2025-04-21 19:59:47 +0300

    register için viewmodel oluşturuldu

---
+++ lib/client/viewmodel/register_viewmodel.dart
@@ -0,0 +1,81 @@
+import 'package:dietician_app/client/core/utils/auth_storage.dart';
+import 'package:dietician_app/client/services/auth/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class RegisterViewModel extends ChangeNotifier {
+  final AuthService _authService = AuthService();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  bool _isButtonPressed = false;
+  String? _errorMessage;
+
+  TextEditingController get nameController => _nameController;
+  TextEditingController get emailController => _emailController;
+  TextEditingController get phoneController => _phoneController;
+  TextEditingController get passwordController => _passwordController;
+  bool get isLoading => _isLoading;
+  bool get isButtonPressed => _isButtonPressed;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> register(BuildContext context) async {
+    setLoading(true);
+    setButtonPressed(true);
+
+    try {
+      final response = await _authService.register(
+        name: _nameController.text.trim(),
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+        phone: _phoneController.text.trim(),
+      );
+
+      if (response.success) {
+        await AuthStorage.saveToken(response.data.token);
+        final userId = response.data.user.id;
+
+        Navigator.pushReplacementNamed(context, '/user_info', arguments: userId);
+      } else {
+        throw Exception(response.message);
+      }
+    } catch (e) {
+      _errorMessage = e.toString();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Kayıt başarısız: $_errorMessage")),
+      );
+    } finally {
+      setLoading(false);
+      setButtonPressed(false);
+    }
+  }
+
+  void setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
+  void setButtonPressed(bool value) {
+    _isButtonPressed = value;
+    notifyListeners();
+  }
+
+  void clearControllers() {
+    _nameController.clear();
+    _emailController.clear();
+    _phoneController.clear();
+    _passwordController.clear();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
